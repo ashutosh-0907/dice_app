fe801072a995b1436f60223e26b4ce2bb2296e02
ashutosz <ashutoshrai08022003@gmail.com>
2023-04-13 19:44:14 +0530

    dice app3

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,17 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice6.png'),          
-        )
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+          Expanded(
+            child: Image.asset('images/dice6.png'),          
+          )
+        ],
+      ),
     );
   }
 }
